7ac1ee40811a7562d72278c8ca53d7e100db2b5f
YoungHo-Jo <zeroho.jo@gmail.com>
2018-10-03 21:07:27 +0900

    Change request module of switch

--- FrontEnd/src/modules/switch.js
+++ FrontEnd/src/modules/switch.js
@@ -1,3 +1,4 @@
+import request from 'superagent'
 module.exports = {
   setSwitch: setSwitchState
 }
@@ -15,16 +16,10 @@ const plugNameMapping = {
  * The name of the plug, not the MAC Address
  * @param toState
  * The state of wanting to change
+ * @param success
+ * A callback function called when the request is succeed
  */
-function setSwitchState(plugName, toState) {
-  var header = {
-    'Accept': 'application/json',
-    'X-M2M-RI': '12345',
-    'X-M2M-Origin': 'Ssmart-home',
-    'content-type': 'application/vnd.onem2m-res+json; ty=4',
-    'Content-Type': 'application/vnd.onem2m-res+json; ty=4',
-    'Content-type': 'application/vnd.onem2m-res+json; ty=4'
-  }
+function setSwitchState(plugName, toState, success) {
 
   var body = {
     "m2m:cin": {
@@ -37,19 +32,19 @@ function setSwitchState(plugName, toState) {
   }
   body["m2m:cin"]["con"]["cmd"][plugNameMapping[plugName]] = toState 
 
-  var options = {
-    url: 'http://52.78.33.177:7579/Mobius/smart-home/switch',
-    method: 'POST',
-    headers: header,
-    form: JSON.stringify(body)
-  }
-
-  request(options, (err, res, body) => {
-    if(!err) {
-      console.log(res.statusCode)
-      console.log(res.body)
-    } else {
-
-    }
-  })
+  request
+    .protocols('http://52.78.33.177:7579/Mobius/smart-home/switch')
+    .send(JSON.stringify(body))
+    .set('Accept', 'application/json')
+    .set('X-M2M-RI', '12345')
+    .set('X-M2M-Origin', 'user')
+    .set('content-type', 'application/vnd.onem2m-res+json; ty=4')
+    .set('Content-Type', 'application/vnd.onem2m-res+json; ty=4')
+    .set('Content-type', 'application/vnd.onem2m-res+json; ty=4')
+    .end((err, res) => {
+      if(err) console.log(err)
+      else {
+        success(res)
+      }
+    })
 }
